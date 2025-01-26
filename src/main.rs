28d6ca7844c35a91ef72235f0075187e387e4c33
caiogmrocha <caioguilherme739@gmail.com>
2025-01-25 22:22:34 -0300

    feat: create setup Startup system

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::{
+    env,
+    fs::File,
+    io,
+};
+
+use projeto_face_vertice::read_off_file;
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let default = String::from("assets/triangles.off");
+
+    let file_path: &str = args.get(1).unwrap_or_else(|| &default).as_str();
+
+    let file = File::open(file_path)?;
+
+    let (_vertices, _faces) = read_off_file(file);
+
+    Ok(())
+}
